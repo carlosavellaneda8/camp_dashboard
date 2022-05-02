063bdb3ed111aec3b4364ecb7d8bbccff8be6fbc
carlosavellaneda8 <carlos.avellaneda8@gmail.com>
2022-05-01 23:12:57 -0500

    Include security via single password

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,35 @@ def get_data():
     return etl.run()
 
 
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store password
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show input for password.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 Password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
+
+
 def filter_data(input_dataset: pd.DataFrame, column: str, value: str) -> pd.DataFrame:
     """Function to filter a dataframe"""
     output = input_dataset.copy()
@@ -54,42 +83,43 @@ st.set_page_config(
     layout="wide",
 )
 
-# Retrieving the data
-dataset = get_data()
+if check_password():
+    # Retrieving the data
+    dataset = get_data()
 
-# Dashboard content
-st.title("Retiro Nacional de Jóvenes ILBD 2022 - Reporte de pagos")
+    # Dashboard content
+    st.title("Retiro Nacional de Jóvenes ILBD 2022 - Reporte de pagos")
 
-ministry_list = ["Todos"] + dataset.ministerio_obra.drop_duplicates().tolist()
-mission_list = ["Todos"] + dataset.detalle_obra.drop_duplicates().tolist()
-mission_list = [obj for obj in mission_list if obj is not np.nan]
+    ministry_list = ["Todos"] + dataset.ministerio_obra.drop_duplicates().tolist()
+    mission_list = ["Todos"] + dataset.detalle_obra.drop_duplicates().tolist()
+    mission_list = [obj for obj in mission_list if obj is not np.nan]
 
-with st.sidebar:
-    main_filter = st.selectbox("Filtrar por ministerio/obra:", ministry_list)
-    if main_filter == "Obra/iglesia hija":
-        secondary_filter = st.selectbox("Obra o iglesia hija:", mission_list)
-    else:
-        secondary_filter = None
+    with st.sidebar:
+        main_filter = st.selectbox("Filtrar por ministerio/obra:", ministry_list)
+        if main_filter == "Obra/iglesia hija":
+            secondary_filter = st.selectbox("Obra o iglesia hija:", mission_list)
+        else:
+            secondary_filter = None
 
-filtered_dataset = filter_data(input_dataset=dataset, column="ministerio_obra", value=main_filter)
+    filtered_dataset = filter_data(input_dataset=dataset, column="ministerio_obra", value=main_filter)
 
-if secondary_filter:
-    filtered_dataset = filter_data(
-        input_dataset=filtered_dataset, column="detalle_obra", value=secondary_filter
-    )
+    if secondary_filter:
+        filtered_dataset = filter_data(
+            input_dataset=filtered_dataset, column="detalle_obra", value=secondary_filter
+        )
 
-st.markdown(f"""
-### Total de personas inscritas
+    st.markdown(f"""
+    ### Total de personas inscritas
 
-El total de personas inscritas es {filtered_dataset["número_de_documento"].drop_duplicates().size}
+    El total de inscritos es {filtered_dataset["número_de_documento"].drop_duplicates().size}
 
-### Valor total recaudado
+    ### Valor total recaudado
 
-El valor total recaudado es de ${filtered_dataset.total_abono.sum():,.0f}
-""")
+    El valor total recaudado es de ${filtered_dataset.total_abono.sum():,.0f}
+    """)
 
-week_data = week_summary(input_dataset=filtered_dataset)
-person_dataset = person_summary(input_dataset=filtered_dataset)
+    week_data = week_summary(input_dataset=filtered_dataset)
+    person_dataset = person_summary(input_dataset=filtered_dataset)
 
-st.bar_chart(week_data)
-st.dataframe(person_dataset)
+    st.bar_chart(week_data)
+    st.dataframe(person_dataset)
